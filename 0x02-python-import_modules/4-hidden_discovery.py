1fa09f73bc2a81fe8195fcffc01c827af731faa6
Queenette23 <97518755+Queenette23@users.noreply.github.com>
2024-01-18 15:08:34 +0100

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-if __name__ == "__main__":
-    import hidden_4
-    hidden = dir(hidden_4)
-
-    for file in hidden:
-        if file.startswith("__") is not True:
-            print("{}".format(file))
